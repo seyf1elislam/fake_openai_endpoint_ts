efc9666c4ab3eaa054df03df67d39f2177bc0b46
O S I H <40665383+seyf1elislam@users.noreply.github.com>
2024-01-30 21:11:05 +0100

    Edits

--- src/request_handlers/handleCompeletionRequest.ts
+++ src/request_handlers/handleCompeletionRequest.ts
@@ -1,11 +1,19 @@
-import { json } from "stream/consumers";
-import { delay, get_header_token } from "./utils";
+import {
+  delay,
+  get_header_token,
+  wrap_textchunk_in_openai_jsonformat,
+} from "./utils";
 import { Response, Request } from "express";
+//write default message with 50words
+let default_message: String = `This is a test message that has been sent from the server. to test the streaming feature of the openai api.
+if you are seeing this message then the streaming feature is working fine.
+if you did not see this message then there is a problem with the streaming feature.
+please check the server logs for more information.
+`;
 export async function handleCompletionRequest(
   request: Request,
   response: Response
 ) {
-  
   console.log(" compeletion Called ------------------");
   let header_token = get_header_token(request);
 
@@ -18,30 +26,33 @@ export async function handleCompletionRequest(
     console.log("socket closed ------------------");
   });
   //?===========================================
-  //?===========================================
-  let bot = request.body.model;
-  const streaming = request.body.stream ?? false;
+  let bot: String = request.body.model;
+  const streaming: Boolean = request.body.stream ?? false;
 
   await delay(100);
   await delay(100);
   if (streaming) {
     try {
-      let reply = "";
-      // while (!isGenerationStopped) {
-      // if (response.headersSent === false) {
-      //   response.writeHead(200, {
-      //     "Content-Type": "text/event-stream; charset=utf-8",
-      //     "Transfer-Encoding": "chunked",
-      //     Connection: "keep-alive",
-      //     "Cache-Control": "no-cache",
-      //     });
-      // }
-      await delay(50);
-      // response.write(json + "\n\n", "utf-8");
-      response.write(`data: ${json}\n\n`, "utf-8");
-      // reply = {   choices: [{ message: { content: reply.replace(/_/g, "*") } }] };
-      console.log("sent chunk ------------------>");
-
+      let message_chunk: Array<String> = default_message.split(" ");
+      message_chunk.forEach(async (chunk: String) => {
+        if (response.headersSent === false) {
+          response.writeHead(200, {
+            "Content-Type": "text/event-stream; charset=utf-8",
+            // "Content-Type": "application/json;charset=utf-8",
+            "Transfer-Encoding": "chunked",
+            Connection: "keep-alive",
+            "Cache-Control": "no-cache",
+          });
+        }
+        // let reply = wrap_textchunk_in_openai_jsonformat(bot, chunk);
+        let dic = { choices: [{ message: { content: chunk } }] };
+        let reply = JSON.stringify(dic);
+        response.write(`data: ${reply}`, "utf-8");
+        await delay(1000);
+        // response.write(`data: ${reply}\n\n`, "utf-8");
+        // response.send(`data: ${reply}\n\n`, "utf-8");
+        console.log("sent chunk ------------------>" + chunk);
+      });
       //!===========================================
       //!===========================================
     } catch (err) {
@@ -52,12 +63,12 @@ export async function handleCompletionRequest(
   } else {
     //? non streaming mode =================================================
     try {
-      await delay(200);
+      await delay(1000);
       // Wrap it back to OAI format
-      const msg = "this is reply";
       let reply: any = {
-        choices: [{ message: { content: msg.replace(/_/g, "*") } }],
+        choices: [{ message: { content: default_message } }],
       };
+
       response.send(reply);
     } catch {
       return response.sendStatus(500);
--- src/request_handlers/handleModelsRequest.ts
+++ src/request_handlers/handleModelsRequest.ts
@@ -1,52 +1,46 @@
-export async function handleModelsRequest(request, response)  {
-    console.log(" models Called ------------------");
-    
-    //       return response.sendStatus(401);
-    // } else if (config.token === empty_token_placeholder) {
-    //   config.token = header_token;
-    //   cach_config(config);
-    // }
+export async function handleModelsRequest(request, response) {
+  console.log(" models Called ------------------");
+
+  //       return response.sendStatus(401);
+  // } else if (config.token === empty_token_placeholder) {
+  //   config.token = header_token;
+  //   cach_config(config);
+  // }
   try {
-      let botNames = [
-        "Chatbot",
-        "DialoGPT",
-        "Ada",
-        "Babbage",
-        "Curie",
-      ]
-      const data = [];
-      botNames.map(function (element) {
-        data.push({
-          id: element,
-          object: "model",
-          created: 1669599635,
-          owned_by: "POEPlugine-internal",
-          permission: [
-            {
-              id: "modelperm-jepinXYt59ncUQrjQEIUEDyC",
-              object: "model_permission",
-              created: 1688551385,
-              allow_create_engine: false,
-              allow_sampling: true,
-              allow_logprobs: true,
-              allow_search_indices: false,
-              allow_view: true,
-              allow_fine_tuning: false,
-              organization: "*",
-              group: null,
-              is_blocking: false,
-            },
-          ],
-          root: element,
-          parent: null,
-        });
-        return null;
+    let botNames = ["Chatbot", "Chatbot2", "Chatbot3", "Chatbot4"];
+    const data = [];
+    botNames.map(function (element) {
+      data.push({
+        id: element,
+        object: "model",
+        created: 1669599635,
+        owned_by: "OpenAI-internal",
+        permission: [
+          {
+            id: "modelperm-jepinXYt59ncUQrjQEIUEDyC",
+            object: "model_permission",
+            created: 1688551385,
+            allow_create_engine: false,
+            allow_sampling: true,
+            allow_logprobs: true,
+            allow_search_indices: false,
+            allow_view: true,
+            allow_fine_tuning: false,
+            organization: "*",
+            group: null,
+            is_blocking: false,
+          },
+        ],
+        root: element,
+        parent: null,
       });
-      console.log("Poe is Connected! ---------------");
-      response.json({ object: "list", data });
-    } catch (err) {
-      console.error(err);
-  
-      return response.sendStatus(401);
-    }
+      return null;
+    });
+
+    response.json({ object: "list", data });
+  } catch (err) {
+    console.error(err);
+
+    return response.sendStatus(401);
   }
+}
